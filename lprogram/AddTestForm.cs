28e5e82e4e18a44fec734facf45fcc3353bf06a5
mfreil <maryloufreil@gmail.com>
2015-03-13 10:59:31 -0400

    Add test form

---
+++ lprogram/AddTestForm.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lprogram
+{
+    public partial class AddTestForm : Form
+    {
+        int MovBox = 1;
+        int ColBox = 1;
+        Dictionary<int, TextBox> movementBoxes;
+        Dictionary<int, TextBox> collectiveBoxes;
+        Dictionary<int, Label> labels;
+
+        public AddTestForm()
+        {
+            InitializeComponent();
+        }
+         
+        private void addMov_Click(object sender, EventArgs e)
+        {
+            // Validate that all fields have been filled out
+
+            // Add Boxes
+            AddMovementBoxes(Convert.ToInt16( numMov.Text));
+            AddCollectiveBoxes(Convert.ToInt16(numColl.Text));
+            // Add Save button
+            AddSaveButton();
+        }
+
+        public System.Windows.Forms.TextBox AddMovementBoxes(int numBoxes)
+        {
+            int left = 50;
+            int topOffset = 1;
+            movementBoxes = new Dictionary<int, TextBox>();
+            labels = new Dictionary<int, Label>();
+            for (int i = 1; i <= numBoxes; i++ )
+            {
+                System.Windows.Forms.TextBox txt = new System.Windows.Forms.TextBox();
+                movementBoxes.Add(i, txt);
+                System.Windows.Forms.Label lbl = new System.Windows.Forms.Label();
+                this.Controls.Add(lbl);
+                labels.Add(i, lbl);
+                lbl.Text = this.MovBox.ToString();
+                lbl.Left = left;
+                lbl.Top = 200 + topOffset * 25;
+                lbl.Width = 20;
+                this.Controls.Add(txt);
+                txt.Top = 200 + topOffset * 25;
+                txt.Left = left + 25;
+                txt.Text = "1";
+                txt.Width = 25;
+                MovBox += 1;
+                topOffset += 1;
+                if (MovBox == 20)
+                {
+                    left = left + 100;
+                    topOffset = 1;
+                }
+  
+            }
+            return null;
+        }
+        public System.Windows.Forms.TextBox AddCollectiveBoxes(int numBoxes)
+        {
+            int left = 300;
+            int topOffset = 1;
+            collectiveBoxes = new Dictionary<int, TextBox>();
+            for (int i = 1; i <= numBoxes; i++)
+            {
+                System.Windows.Forms.Label lbl = new System.Windows.Forms.Label();
+                this.Controls.Add(lbl);
+                labels.Add(i+MovBox, lbl);
+                lbl.Text = this.ColBox.ToString();
+                lbl.Left = left;
+                lbl.Top = 200 + topOffset * 25;
+                lbl.Width = 20;
+                System.Windows.Forms.TextBox txt = new System.Windows.Forms.TextBox();
+                collectiveBoxes.Add(i, txt);
+                this.Controls.Add(txt);
+                txt.Top =200 + ColBox * 25;
+                txt.Left = left+25 ;
+                txt.Text = "1";
+                txt.Width = 25;
+                ColBox += 1;
+                topOffset += 1;
+             }
+            return null;
+        }
+        public System.Windows.Forms.Button AddSaveButton()
+        {
+            System.Windows.Forms.Button btn = new System.Windows.Forms.Button();
+            this.Controls.Add(btn);
+            btn.Top = 700;
+            btn.Left = 50;
+            btn.Text = "Save";
+            btn.Name = "BtnOK";
+            btn.Click += new System.EventHandler(btnOK_Click);
+            btn = new System.Windows.Forms.Button();
+            this.Controls.Add(btn);
+            btn.Top = 700;
+            btn.Left = 150;
+            btn.Text = "Cancel";
+            btn.Name = "BtnCancel";
+            btn.Click += new System.EventHandler(btnCancel_Click);
+            return btn;
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            // See if there is a test with this name already in database. If so, error
+            if (Tests.testIDbyName(testName.Text) > 0)
+            {
+                MessageBox.Show("A test with this name already exists, please choose another");
+                this.DialogResult = DialogResult.None;
+                return;
+            }
+            // Create a test in database
+            Tests newTest = new Tests();
+            newTest.setNumMovements(int.Parse(numMov.Text));
+            newTest.setNumCollectives(int.Parse(numColl.Text));
+            newTest.setTotalPoints(int.Parse(totalPts.Text));
+            newTest.setTestName(testName.Text);
+            Dictionary<int, int> testData = new Dictionary<int, int>();
+            foreach (KeyValuePair<int,TextBox> item in movementBoxes)
+            {
+                testData.Add(item.Key, int.Parse(item.Value.Text));
+            }
+            newTest.setMovements(testData);
+            Dictionary<int, int> collData = new Dictionary<int, int>();
+            foreach (KeyValuePair<int, TextBox> item in collectiveBoxes)
+            {
+                collData.Add(item.Key, int.Parse(item.Value.Text));
+            }
+            newTest.setCollectives(collData);
+            newTest.saveTest();
+            clearForm();
+            this.DialogResult = DialogResult.OK;
+        }
+        private void clearForm()
+        {
+            removeBoxes();
+            numColl.Text = "";
+            numMov.Text = "";
+            testName.Text = "";
+            totalPts.Text = "";
+            MovBox = 1;
+            ColBox = 1;
+        }
+        private void removeBoxes()
+        {
+            foreach (KeyValuePair<int, TextBox> item in movementBoxes)
+            {
+                this.Controls.Remove(item.Value);
+                item.Value.Dispose();
+            }
+            foreach (KeyValuePair<int, TextBox> item in collectiveBoxes)
+            {
+                this.Controls.Remove(item.Value);
+                item.Value.Dispose();
+            }
+            foreach (KeyValuePair<int,Label> item in labels)
+            {
+                this.Controls.Remove(item.Value);
+                item.Value.Dispose();
+            }
+        }
+    }
+}
---
+++ lprogram/Tests.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Forms;
+
+namespace lprogram
+{
+    class Tests
+    {
+        int testID;
+        int numMov;
+        int numCol;
+        int totalPoints;
+        string testName;
+        Dictionary<int,int> movementIDs;
+        Dictionary<int,int> collectiveIDs;
+
+        public Tests() 
+        {
+            testID = 0;
+            numMov = 0;
+            numCol = 0;
+           
+        }
+        public Tests(int ID)
+        {
+            testID = ID;
+            // Get the Test information
+            SQLiteDatabase db = new SQLiteDatabase();
+            DataTable test;
+            String query = String.Format("select * from Tests where ID = {0}", testID);
+            test = db.GetDataTable(query);
+            if (test != null)
+            {
+                numMov = int.Parse(test.Rows[0]["NumMovements"].ToString());
+                numCol = int.Parse(test.Rows[0]["NumCollectives"].ToString());
+                totalPoints = int.Parse(test.Rows[0]["TotalPoints"].ToString());
+                testName = test.Rows[0]["Name"].ToString();
+            }
+            // Get the movements
+            DataTable movements;
+            query = String.Format("select Coeff from Movements where TestID={0} and  Collective!=1 order by MovementNo;", testID);
+            movements = db.GetDataTable(query);
+            movementIDs = new Dictionary<int, int>();
+            int count = 0;
+            if (movements != null)
+            {
+                foreach (DataRow r in movements.Rows)
+                {
+                    movementIDs.Add(count, int.Parse(r["Coeff"].ToString()));
+                    count++;
+                }
+            }
+            query = String.Format("select Coeff from Movements where TestID={0} and  Collective==1 order by MovementNo;", testID);
+
+            movements = db.GetDataTable(query);
+            collectiveIDs = new Dictionary<int, int>();
+            count = 0;
+            if (movements != null)
+            {
+                foreach (DataRow r in movements.Rows)
+                {
+                    collectiveIDs.Add(count, int.Parse(r["Coeff"].ToString()));
+                    count++;
+                }
+            }
+        }
+        //Need getters
+        public int getTestID()
+        {
+            return testID;
+        }
+        public int getNumMovements()
+        {
+            return numMov;
+        }
+        public int getNumCollectives()
+        {
+            return numCol;
+        }
+        public int getTotalPoints()
+        {
+            return totalPoints;
+        }
+        public Dictionary<int,int> getMovements()
+        {
+            return movementIDs;
+        }
+        public string getTestName()
+        {
+            return testName;
+        }
+        public Dictionary<int,int> getCollectives()
+        {
+            return collectiveIDs;
+        }
+        //Need setters
+        public int setTestID(int id)
+        {
+            testID = id;
+            return 0;
+        }
+        public int setNumMovements(int num)
+        {
+            numMov = num;
+            return 0;
+        }
+        public int setNumCollectives(int num)
+        {
+            numCol = num;
+            return 0;
+        }
+        public int setTotalPoints(int num)
+        {
+            totalPoints = num;
+            return 0;
+        }
+        public int setTestName(string str)
+        {
+            testName = str;
+            return 0;
+        }
+        public int setMovements(Dictionary<int,int> dict)
+        {
+            movementIDs = dict;
+            return 0;
+        }
+        public int setCollectives(Dictionary<int,int> dict)
+        {
+            collectiveIDs = dict;
+            return 0;
+        }
+        public int saveTest()
+        {
+            // Save test if testId == 0; Otherwise update
+            //Check to see if there is a test with this name
+            SQLiteDatabase db = new SQLiteDatabase();
+            Dictionary<string, string> data = new Dictionary<string, string>();
+            if (testID == 0)
+            { 
+                data.Add("NumMovements", numMov.ToString());
+                data.Add("NumCollectives", numCol.ToString());
+                data.Add("TotalPoints", totalPoints.ToString());
+                data.Add("Name", testName.ToString());
+                try
+                {
+                    db.Insert("Tests", data);
+                }
+                catch (Exception crap)
+                {
+                    MessageBox.Show(crap.Message);
+                    return 1;
+                }
+                // Get the id of test just added
+                String query = string.Format("select ID from Tests where Name='{0}'", testName);
+                testID = int.Parse(db.ExecuteScalar(query));
+                Dictionary<string, string> testData;
+                // Use test id to save movements
+                foreach (KeyValuePair<int, int> item in movementIDs)
+                {
+                    testData = new Dictionary<string, string>();
+                    testData.Add("MovementNo", item.Key.ToString());
+                    testData.Add("Coeff", item.Value.ToString());
+                    testData.Add("TestID", testID.ToString());
+
+                    try
+                    {
+                        db.Insert("Movements", testData);
+                    }
+                    catch (Exception crap)
+                    {
+                        MessageBox.Show(crap.Message);
+                        return 1;
+                    }
+                }
+                foreach(KeyValuePair<int,int> item in collectiveIDs)
+                {
+                    testData = new Dictionary<string, string>();
+                    testData.Add("MovementNo", item.Key.ToString());
+                    testData.Add("Coeff", item.Value.ToString());
+                    testData.Add("TestID", testID.ToString());
+                    testData.Add("Collective", "1");
+                   
+                    try
+                    {
+                        db.Insert("Movements", testData);
+                    }
+                    catch (Exception crap)
+                    {
+                        MessageBox.Show(crap.Message);
+                        return 1;
+                    }
+
+                }
+            }
+  
+            return 0;
+        }
+        // Static functions
+        public static int testIDbyName(string name)
+        {
+            SQLiteDatabase db = new SQLiteDatabase();
+            String query = string.Format("select ID from Tests where Name='{0}'", name);
+            string result = db.ExecuteScalar(query);
+            if (result=="")
+            {
+                return 0;
+            }
+            return int.Parse(result);
+        }
+        public static DataTable getTestNames()
+        {
+            SQLiteDatabase db = new SQLiteDatabase();
+            DataTable testNames;
+            String query = "select Name from Tests;";
+            testNames = db.GetDataTable(query);
+            return testNames;
+        }
+    }
+}
